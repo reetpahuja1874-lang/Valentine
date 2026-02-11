ac6d575f739977bf73852db4a539759b88df6332
reetpahuja1874-lang <reet.pahuja1874@gmail.com>
2026-02-11 15:56:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function nextPage(num) {
+  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+  document.getElementById("page" + num).classList.add("active");
+}
 
+let noScale = 1;
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.onclick = () => {
+  nextPage(3);
+};
+
+noBtn.onclick = () => {
+  noScale -= 0.15;
+  noBtn.style.transform = `scale(${noScale})`;
+
+  if (noScale <= 0.3) {
+    noBtn.style.display = "none";
+  }
+};
